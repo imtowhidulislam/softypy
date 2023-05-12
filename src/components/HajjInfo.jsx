efac73aafb91a8d281c8f080d0f7c7f3129ca14a
towhid <niamaaktar@gmail.com>
2023-05-13 01:26:25 +0600

    all kind of small issues has been resolved

--- src/components/HajjInfo.jsx
+++ src/components/HajjInfo.jsx
@@ -13,12 +13,12 @@ const HajjInfo = () => {
     <div>
         <div className='grid place-items-center'>
             <div className='container rounded-2xl overflow-hidden'>
-                <div className='bg-gray-200 grid place-items-center  md:flex md:justify-center gap-8 border-2 border-red-200'>
-                    <div className='flex items-center gap-3 py-4 px-4'>
+                <div className='bg-gray-200 grid place-items-center  md:flex md:justify-center gap-8'>
+                    <div className='flex items-center gap-3 py-4 px-4 md:w-full'>
                         <img src={Hajj} alt="Macca" />
                         <h2 className='font-bold text-base'>Hajj & Umrah</h2>
                     </div>
-                    <nav className='list-none flex items-center justify-between w-full'>
+                    <nav className='list-none flex items-center justify-between w-full flex-wrap md:flex-nowrap'>
                         <li className='text-base md:text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><TiPlaneOutline/><span className='text-sm font-bold '>Trip</span></a></li>
                         <li className='text-base md:text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaHotel /><span className='text-sm font-bold '>Hotel</span></a></li>
                         <li className='text-base md:text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaCcVisa/><span className='text-sm font-bold '>Card</span></a></li>
