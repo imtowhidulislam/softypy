86f7f9d93a68e8c06c81d2e2656ddf26d89189f2
towhid <niamaaktar@gmail.com>
2023-05-13 00:44:21 +0600

    all kind of small issues has been resolved

--- src/components/HajjInfo.jsx
+++ src/components/HajjInfo.jsx
@@ -13,22 +13,22 @@ const HajjInfo = () => {
     <div>
         <div className='grid place-items-center'>
             <div className='container rounded-2xl overflow-hidden'>
-                <div className='bg-gray-200 flex justify-center gap-12 px-4'>
+                <div className='bg-gray-200 flex justify-center gap-8 px-4'>
                     <div className='flex w-full items-center gap-3 py-4 px-4'>
                         <img src={Hajj} alt="Macca" />
-                        <h2 className='font-bold text-xl'>Hajj & Umrah</h2>
+                        <h2 className='font-bold text-base'>Hajj & Umrah</h2>
                     </div>
                     <nav className='list-none flex items-center justify-between w-full'>
-                        <li className='text-3xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><TiPlaneOutline/><span className='text-xl font-bold '>Trip</span></a></li>
-                        <li className='text-3xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaHotel /><span className='text-xl font-bold '>Hotel</span></a></li>
-                        <li className='text-3xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaCcVisa/><span className='text-xl font-bold '>Card</span></a></li>
-                        <li className='text-3xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaTrain /><span className='text-xl font-bold '>Train</span></a></li>
-                        <li className='text-3xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaBusAlt /><span className='text-xl font-bold '>Bus</span></a></li>
-                        <li className='text-3xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><GiFootTrip /><span className='text-xl font-bold '>Travel</span></a></li>
+                        <li className='text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><TiPlaneOutline/><span className='text-sm font-bold '>Trip</span></a></li>
+                        <li className='text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaHotel /><span className='text-sm font-bold '>Hotel</span></a></li>
+                        <li className='text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaCcVisa/><span className='text-sm font-bold '>Card</span></a></li>
+                        <li className='text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaTrain /><span className='text-sm font-bold '>Train</span></a></li>
+                        <li className='text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><FaBusAlt /><span className='text-sm font-bold '>Bus</span></a></li>
+                        <li className='text-2xl text-button bg-transparent hover:bg-cyan-200 trasi ease duration-300 py-4 px-4'><a className=" flex items-center gap-3" href="#"><GiFootTrip /><span className='text-sm font-bold '>Travel</span></a></li>
                     </nav>
                 </div>
-            <div className=''>
-                <div className='shadow-shadowOne px-12 py-20 grid grid-cols-responsiveLayoutHajj gap-3 md:grid-cols-responsiveLayoutHajjDefault bg-[#e5e7e55e]'>
+            <div className='px-4 shadow-shadowOne  md:px-0 relative bg-[#e7e7e75c] py-8'>
+                <div className='px-12 pb-8 grid grid-cols-responsiveLayoutHajj gap-3 md:grid-cols-responsiveLayoutHajjDefault '>
                     <div className='bg-slate-200 p-2 rounded-md'>
                         <h2>City/Hotel/Street Name</h2>
                         <p>Enter your city</p>
@@ -57,10 +57,10 @@ const HajjInfo = () => {
                         <h2>Rooms & Guests</h2>
                         <p>1 Person</p>
                     </div>
-                    <div className='grid place-items-center'>
+                </div>
+                    <div className='flex items-center justify-center w-full'>
                         <Button textBtn="search hotels" />
                     </div>
-                </div>
                 <div className='whatsup border-4 border-button p-2 w-max rounded-full'>
                     <FaWhatsapp className='text-5xl text-button' />
                 </div>
